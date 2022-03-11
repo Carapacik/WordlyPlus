db333650f693f392d6a874250a13ceb56ee17df1
Carapacik <carapacik@gmail.com>
2022-03-11 20:59:06 +0300

    Add en dictionary to ae

---
+++ lib/utils/dictionary_ru.dart
@@ -0,0 +1,63 @@
+const Map<String, String> dictionaryRu = {
+  'aback': 'To be very shocked or surprised',
+  'abaft': 'At the back of or behind a ship or boat',
+  'abase':
+      'To make yourself seem to be less important or to not deserve respect',
+  'abash': 'To embarrass someone or make them feel uncomfortable',
+  'abate': 'To become less strong',
+  'abaya':
+      'A long piece of clothing that reaches to the ground, covering the whole of the body except the head, feet, and hands, worn by some Muslim women',
+  'abbey':
+      'A building where monks or nuns live or used to live. Some abbeys are now used as churches',
+  'abbot': 'A man who is in charge of a monastery',
+  'abhor':
+      'To hate a way of behaving or thinking, often because you think it is not moral',
+  'abide':
+      "If you can't abide someone or something, you dislike them very much",
+  'abled':
+      'Not having a disability (= an illness, injury, or condition that makes it difficult for someone to do some things that other people do)',
+  'abode': 'The place where someone lives',
+  'abort':
+      'To cause something to stop or fail before it begins or before it is complete',
+  'about': 'On the subject of, or connected with',
+  'above': 'In or to a higher position than something else',
+  'abuse':
+      'To use something for the wrong purpose in a way that is harmful or morally wrong',
+  'abuzz': 'Filled with noise and activity',
+  'abyss': 'A very deep hole that seems to have no bottom',
+  'ached': 'To have a continuous pain that is unpleasant but not very strong',
+  'ackee':
+      'A fruit similar in appearance to a large pear, which is popular in Jamaica cooked as a savoury',
+  'acorn':
+      'An oval nut that grows on an oak tree and has an outer part shaped like a cup',
+  'acrid':
+      'An acrid smell or taste is strong and bitter and causes a burning feeling in the throat',
+  'actor':
+      'Someone who pretends to be someone else while performing in a film, play, or television or radio programme',
+  'acute': 'If a bad situation is acute, it causes severe problems or damage',
+  'adage': 'A wise saying',
+  'adapt':
+      'To change, or to change something, to suit different conditions or uses',
+  'adder': 'A type of poisonous snake',
+  'addle': 'To make someone feel confused and unable to think clearly',
+  'adept': 'Having a natural ability to do something that needs skill',
+  'adieu': 'Goodbye',
+  'adios': 'Goodbye',
+  'adman': 'A man who works in advertising',
+  'admin':
+      'The activities involved in managing or organizing a business or other organization',
+  'admit': 'To agree that something is true, especially unwillingly',
+  'adobe':
+      'A mixture of earth and straw made into bricks and dried in the sun, used to build houses in some parts of the world',
+  'adopt':
+      "To legally take another person's child into your own family and take care of him or her as your own child",
+  'adore':
+      'To love someone very much, especially in a way that shows a lot of admiration or respect, or to like something very much',
+  'adorn': 'To add something decorative to a person or thing',
+  'adult': 'A person or animal that has grown to full size and strength',
+  'aegis':
+      'With the protection or support of someone or something, especially an organization',
+  'aerie':
+      'The nest of an eagle or other large bird that eats meat, usually built in a high place that cannot be easily reached',
+};
+// ae

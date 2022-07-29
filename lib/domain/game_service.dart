e646e6fcdd6c1540c727ab183a6669e8c8d2c03c
Carapacik <carapacik@gmail.com>
2022-07-29 10:48:56 +0300

    Fill game service

--- lib/domain/game_service.dart
+++ lib/domain/game_service.dart
@@ -1,3 +1,5 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 class GameService {
   GameService();
 
@@ -6,4 +8,36 @@ class GameService {
 
   static const _levelBoard = 'level_board_key';
   static const _levelNumber = 'level_number_key';
+
+  Future<void> saveDailyWord(String word) async {
+    final sp = await SharedPreferences.getInstance();
+    await sp.setString(_dailyWord, word);
+  }
+
+  Future<String?> getDailyWord() async {
+    final sp = await SharedPreferences.getInstance();
+    return sp.getString(_dailyWord);
+  }
+
+  // TODO replace board type from String to BoardData
+  Future<void> saveDailyBoard(String board) async {
+    final sp = await SharedPreferences.getInstance();
+    await sp.setString(_dailyBoard, board);
+  }
+
+  // TODO replace board type from String to BoardData
+  Future<String> getDailyBoard() async {
+    final sp = await SharedPreferences.getInstance();
+    return sp.getString(_dailyBoard) ?? '';
+  }
+
+  Future<void> saveLevelNumber(int number) async {
+    final sp = await SharedPreferences.getInstance();
+    await sp.setInt(_levelNumber, number);
+  }
+
+  Future<int> getLevelNumber() async {
+    final sp = await SharedPreferences.getInstance();
+    return sp.getInt(_levelNumber) ?? 1;
+  }
 }

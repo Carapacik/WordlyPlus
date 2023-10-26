bf116e8f01d48d6c647b6d7b4ad80c298bd1af88
Roman Laptev <carapacik@gmail.com>
2023-10-26 13:31:06 +0300

    Levels page

---
+++ lib/src/feature/level/widget/level_dialog.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:wordly/src/core/extension/extensions.dart';
+
+Future<void> showLevelDialog(
+  BuildContext context, {
+  required String word,
+  required String meaning,
+  required bool isWin,
+}) =>
+    showDialog<void>(
+      context: context,
+      builder: (context) {
+        final width = MediaQuery.sizeOf(context).width;
+        final padding = width > 350 ? (width - 350) / 2 : 8;
+        return Dialog(
+          backgroundColor: isWin ? Colors.green : Colors.red,
+          insetAnimationDuration: const Duration(milliseconds: 800),
+          insetPadding: EdgeInsets.symmetric(horizontal: padding.toDouble()),
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SelectableText(
+                  word.toUpperCase(),
+                  style: context.theme.tmb.copyWith(color: Colors.white),
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  meaning,
+                  style: context.theme.ll.copyWith(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
--- lib/src/feature/level/widget/level_page.dart
+++ lib/src/feature/level/widget/level_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:wordly/src/core/extension/extensions.dart';
+import 'package:wordly/src/feature/components/widget/constraint_screen.dart';
+import 'package:wordly/src/feature/components/widget/not_played.dart';
 import 'package:wordly/src/feature/game/model/game_result.dart';
+import 'package:wordly/src/feature/level/widget/level_dialog.dart';
 
 class LevelPage extends StatefulWidget {
   const LevelPage({required this.dictionary, super.key});
@@ -22,6 +25,76 @@ class _LevelPageState extends State<LevelPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Title(
+      color: Colors.black,
+      title: context.r.levels,
+      child: Scaffold(
+        appBar: AppBar(title: Text(context.r.levels)),
+        body: ConstraintScreen(
+          child: Builder(
+            builder: (context) {
+              if (_levels == null) {
+                return const HaveNotPlayed();
+              }
+              return GridView.builder(
+                padding: const EdgeInsets.all(20),
+                itemCount: _levels!.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+                itemBuilder: (context, index) => _LevelItem(
+                  level: _levels![index],
+                  dictionary: widget.dictionary,
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _LevelItem extends StatelessWidget {
+  const _LevelItem({required this.level, required this.dictionary});
+
+  final GameResult level;
+  final Locale dictionary;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: Material(
+        clipBehavior: Clip.hardEdge,
+        borderRadius: BorderRadius.circular(8),
+        color: level.isWin == null
+            ? Colors.grey
+            : level.isWin!
+                ? Colors.green
+                : Colors.red,
+        child: InkWell(
+          onTap: () async {
+            if (level.isWin != null) {
+              await showLevelDialog(
+                context,
+                word: level.secretWord,
+                isWin: level.isWin!,
+                meaning: context.dependencies.gameRepository.currentDictionary(dictionary)[level.secretWord] ?? '',
+              );
+            }
+          },
+          child: Center(
+            child: Text(
+              '${level.lvlNumber}\n${level.secretWord}',
+              textAlign: TextAlign.center,
+              style: context.theme.tl.copyWith(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }

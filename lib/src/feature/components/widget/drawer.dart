16d3950648b2431dee407dba26b64f328d0fe317
Carapacik <carapacik@gmail.com>
2023-11-30 21:25:02 +0300

    Replace Drawer to NavigationDrawer

--- lib/src/feature/components/widget/drawer.dart
+++ lib/src/feature/components/widget/drawer.dart
@@ -13,101 +13,89 @@ class CustomDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 200,
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.horizontal(right: Radius.circular(16)),
-          color: context.theme.colorScheme.background,
+    return NavigationDrawer(
+      children:  [
+        ListTile(
+          title: Text(context.r.daily, style: const TextStyle(fontWeight: FontWeight.w500)),
+          onTap: () async {
+            Scaffold.of(context).closeDrawer();
+            final navigator = Navigator.of(context);
+            final bloc = context.read<GameBloc>()..add(const GameEvent.changeGameMode(GameMode.daily));
+            await Future<void>.delayed(const Duration(milliseconds: 250));
+            await navigator.pushAndRemoveUntil(
+              PageRouteBuilder<void>(
+                pageBuilder: (context, _, __) => BlocProvider.value(
+                  value: bloc,
+                  child: const GamePage(),
+                ),
+                transitionDuration: Duration.zero,
+                reverseTransitionDuration: Duration.zero,
+              ),
+                  (route) => false,
+            );
+          },
         ),
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ListTile(
-                title: Text(context.r.daily, style: const TextStyle(fontWeight: FontWeight.w500)),
-                onTap: () async {
-                  Scaffold.of(context).closeDrawer();
-                  final navigator = Navigator.of(context);
-                  final bloc = context.read<GameBloc>()..add(const GameEvent.changeGameMode(GameMode.daily));
-                  await Future<void>.delayed(const Duration(milliseconds: 250));
-                  await navigator.pushAndRemoveUntil(
-                    PageRouteBuilder<void>(
-                      pageBuilder: (context, _, __) => BlocProvider.value(
-                        value: bloc,
-                        child: const GamePage(),
-                      ),
-                      transitionDuration: Duration.zero,
-                      reverseTransitionDuration: Duration.zero,
-                    ),
-                    (route) => false,
-                  );
-                },
+        ListTile(
+          title: Text(context.r.levels, style: const TextStyle(fontWeight: FontWeight.w500)),
+          onTap: () async {
+            Scaffold.of(context).closeDrawer();
+            final navigator = Navigator.of(context);
+            final bloc = context.read<GameBloc>()..add(const GameEvent.changeGameMode(GameMode.lvl));
+            await Future<void>.delayed(const Duration(milliseconds: 250));
+            await navigator.pushAndRemoveUntil(
+              PageRouteBuilder<void>(
+                pageBuilder: (context, _, __) => BlocProvider.value(
+                  value: bloc,
+                  child: const GamePage(),
+                ),
+                transitionDuration: Duration.zero,
+                reverseTransitionDuration: Duration.zero,
               ),
-              ListTile(
-                title: Text(context.r.levels, style: const TextStyle(fontWeight: FontWeight.w500)),
-                onTap: () async {
-                  Scaffold.of(context).closeDrawer();
-                  final navigator = Navigator.of(context);
-                  final bloc = context.read<GameBloc>()..add(const GameEvent.changeGameMode(GameMode.lvl));
-                  await Future<void>.delayed(const Duration(milliseconds: 250));
-                  await navigator.pushAndRemoveUntil(
-                    PageRouteBuilder<void>(
-                      pageBuilder: (context, _, __) => BlocProvider.value(
-                        value: bloc,
-                        child: const GamePage(),
-                      ),
-                      transitionDuration: Duration.zero,
-                      reverseTransitionDuration: Duration.zero,
-                    ),
-                    (route) => false,
-                  );
-                },
-              ),
-              ListTile(
-                title: Text(context.r.tutorial, style: const TextStyle(fontWeight: FontWeight.w500)),
-                onTap: () async {
-                  Scaffold.of(context).closeDrawer();
-                  final navigator = Navigator.of(context);
-                  await navigator.push(
-                    MaterialPageRoute<void>(
-                      builder: (context) => const TutorialPage(),
-                    ),
-                  );
-                },
-              ),
-              ListTile(
-                title: Text(context.r.settings, style: const TextStyle(fontWeight: FontWeight.w500)),
-                onTap: () async {
-                  Scaffold.of(context).closeDrawer();
-                  final navigator = Navigator.of(context);
-                  final bloc = context.read<GameBloc>();
-                  await navigator.push(
-                    MaterialPageRoute<void>(
-                      builder: (context) => BlocProvider.value(
-                        value: bloc,
-                        child: const SettingsPage(),
-                      ),
-                    ),
-                  );
-                },
-              ),
-              ListTile(
-                title: Text(context.r.about, style: const TextStyle(fontWeight: FontWeight.w500)),
-                onTap: () async {
-                  Scaffold.of(context).closeDrawer();
-                  final navigator = Navigator.of(context);
-                  await navigator.push(
-                    MaterialPageRoute<void>(
-                      builder: (context) => const AboutPage(),
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
+                  (route) => false,
+            );
+          },
         ),
-      ),
+        ListTile(
+          title: Text(context.r.tutorial, style: const TextStyle(fontWeight: FontWeight.w500)),
+          onTap: () async {
+            Scaffold.of(context).closeDrawer();
+            final navigator = Navigator.of(context);
+            await navigator.push(
+              MaterialPageRoute<void>(
+                builder: (context) => const TutorialPage(),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          title: Text(context.r.settings, style: const TextStyle(fontWeight: FontWeight.w500)),
+          onTap: () async {
+            Scaffold.of(context).closeDrawer();
+            final navigator = Navigator.of(context);
+            final bloc = context.read<GameBloc>();
+            await navigator.push(
+              MaterialPageRoute<void>(
+                builder: (context) => BlocProvider.value(
+                  value: bloc,
+                  child: const SettingsPage(),
+                ),
+              ),
+            );
+          },
+        ),
+        ListTile(
+          title: Text(context.r.about, style: const TextStyle(fontWeight: FontWeight.w500)),
+          onTap: () async {
+            Scaffold.of(context).closeDrawer();
+            final navigator = Navigator.of(context);
+            await navigator.push(
+              MaterialPageRoute<void>(
+                builder: (context) => const AboutPage(),
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
